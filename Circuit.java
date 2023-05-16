aef316378fbe88dcdd009e2d13fbbb3d72ef7f52
Dylan Ma <99204981+arandomdma@users.noreply.github.com>
2023-05-16 14:44:13 -0400

    Create Circuit.java

---
+++ Circuit.java
@@ -0,0 +1,6 @@
+public class Circuit {
+ /** 
+  this will be composed of other components, such as battery, resistors, etc.
+  will ideally be able to have more than two batteries (will figure this out later)
+ */
+}
