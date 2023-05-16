b2a27719593bc930ed4300eca9c7d9452d0d22f0
Dylan Ma <99204981+arandomdma@users.noreply.github.com>
2023-05-16 14:38:17 -0400

    created a component file

---
+++ Component.java
@@ -0,0 +1,13 @@
+public class Component {
+  /** temp private variables
+      store information about the 
+  */
+  
+  /** 
+  a bunch of methods to help calibrate how the various components behave
+  */
+  
+  /** 
+  potential seperate constructors for the battery, circuits, etc
+  */
+}
